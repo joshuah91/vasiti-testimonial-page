152efc9ae59d2c75d4cc8fafca9d494b0ae3c7ec
joshua <joshplexus8@gmail.com>
2021-04-30 23:08:57 +0100

    testing commitment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>Hello this is my vasiti assessment testimonial page</div>
-    <div>testing</div>
+    <div>testing testing</div>
   );
 }
 
